6c6597f731a295c968fc969db5722ce0d0ca1f3d
Eleanor McHugh <eleanor@games-with-brains.com>
2013-04-27 00:41:00 +0100

    simplified node tests

--- node_test.go
+++ node_test.go
@@ -6,18 +6,12 @@ import(
 
 func TestNodeAppend(t *testing.T) {
 	n := &Node{ value: false }
-	switch r := n.Append(true); {
+	switch n.Append(true); {
 	case n.next == nil:
 		t.Fatalf("%v.Append(%v) failed to create a tail node", n, true)
 	case n.next.value == false:
 		t.Fatalf("%v.Append(%v) tail node value is %v", n, true, false)
-	case r != n.next:
-		t.Fatalf("%v.Append(%v) returned node %v is not the same node as the tail", n, true, r)
-	case r.previous != n:
-		t.Fatalf("%v.Append(%v) the head of returned node %v is not the same node as the head", n, true, r)
-	case r.value == false:
-		t.Fatalf("%v.Append(%v) returned node value is %v", n, true, false)
-	case r.previous.value != false:
-		t.Fatalf("%v.Append(%v) head node value is %v", n, true, true)
+	case n.next.previous != n:
+		t.Fatalf("%v.Append(%v) returned node %v is not the same node as the head node", n, true, n)
 	}
 }
